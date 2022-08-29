fff48cc27caef6ad3cad2fae122adf2babd6754d
Anthony Shibitov <anthonyshibitov@gmail.com>
2022-08-29 18:44:43 -0400

    Solve JS 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, repeatCount) {
+    let completedPhrase = '';
+    if(repeatCount < 0){
+        return 'ERROR';
+    }
+    for (let i = 0; i < repeatCount; i++){
+        completedPhrase += phrase;
+    }
+    return completedPhrase;
 };
 
 // Do not edit below this line
