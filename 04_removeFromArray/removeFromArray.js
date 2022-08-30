532fb74eb30df27ae8e1284cf7f0a79097df1ff0
Anthony Shibitov <anthonyshibitov@gmail.com>
2022-08-29 21:18:53 -0400

    Solve JS 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+    //Convert arguments object into an array, so we can do array operations on it
+    const args = [...arguments];
 
+    const finalArray = args[0];
+
+    for(let argCounter = 1; argCounter < args.length; argCounter++){
+        for(let arrayCounter = 0; arrayCounter < finalArray.length; arrayCounter++){
+            if(args[argCounter] === finalArray[arrayCounter]){
+                finalArray.splice(arrayCounter, 1);
+            }
+        }
+    }
+    return finalArray;
 };
 
 // Do not edit below this line
