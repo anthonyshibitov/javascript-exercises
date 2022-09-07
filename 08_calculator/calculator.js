926c8fa6595b609c21b548f17e6c7446123e4822
Anthony Shibitov <anthonyshibitov@gmail.com>
2022-09-07 16:25:37 -0400

    Solve 08 JS

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums.length == 0) return 0;
+	const result = nums.reduce((prev, curr) => prev + curr);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  if (nums.length == 0) return 0;
+	const result = nums.reduce((prev, curr) => prev * curr);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = a;
+  for(let i = 1; i < b; i++){
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = a;
+  if (a === 0) return 1;
+
+  for(let i = a-1; i > 0; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
