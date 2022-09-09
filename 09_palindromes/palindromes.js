e143f1995977c4df411a0f570c4e22c78aa10677
Anthony Shibitov <anthonyshibitov@gmail.com>
2022-09-08 20:32:08 -0400

    Solve JS 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    //Extract only alphabetical values
+    let alphaExtract = [];
 
+    inputString = inputString.toLowerCase();
+
+    for(let i = 0; i < inputString.length; i++){
+        if(inputString[i] >= 'A' && inputString[i] <= 'z') alphaExtract.push(inputString[i]);
+    }
+    console.log(alphaExtract.toString() + " ||| " + alphaExtract.reverse().toString());
+    return (alphaExtract.toString() == alphaExtract.reverse().toString());
 };
 
 // Do not edit below this line
